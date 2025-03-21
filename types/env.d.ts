b92c488d729094d14e37f35f9cdbe3757c2fe8ae
Yutaro Watanabe <yutaro@protonmail.com>
2025-03-22 00:38:02 +0100

    AI scontrino
    temp2
    
    temp
    
    temp
    
    temp
    
    temp-1
    
    temp
    
    temp
    
    AI scrontrino

--- types/env.d.ts
+++ types/env.d.ts
@@ -2,9 +2,12 @@ declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
 			OPENROUTER_API_KEY: string;
+			OPENAI_API_KEY: string;
+			OPENROUTER_API_KEY_PATH: string;
+			OPENAI_API_KEY_PATH: string;
 		}
 	}
 }
 
 // Need to be exported to be a valid module
-export type {};
+export type { };
